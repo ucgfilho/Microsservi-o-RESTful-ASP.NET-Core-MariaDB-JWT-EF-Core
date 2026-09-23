e62079c7bbad2c8832d11ad20bc9e67a8456477c
Ubirajara Filho <ucg.filho@outlook.com>
2026-09-23 12:00:34 -0300

    feat: aplica migração automática se houver tabelas faltantes

--- Program.cs
+++ Program.cs
@@ -80,6 +80,12 @@ builder.Services.AddAuthentication(x =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
